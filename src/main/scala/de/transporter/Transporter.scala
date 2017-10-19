fd5c6a4d8736ae6db897382373a20e7dca20723f
Daniel Schruhl <danielschruhl@gmail.com>
2017-10-19 18:47:43 +0200

    add logging

--- src/main/scala/de/transporter/Transporter.scala
+++ src/main/scala/de/transporter/Transporter.scala
@@ -5,15 +5,21 @@ import akka.http.scaladsl.Http
 import akka.stream.ActorMaterializer
 import com.typesafe.config.ConfigFactory
 import io.prometheus.client.hotspot.DefaultExports
+import org.slf4j.LoggerFactory
 
 object Transporter extends App {
-
+  val log = LoggerFactory.getLogger(Transporter.getClass)
+  log.info("Starting Transporter")
+  log.debug("Loading config")
   val config = ConfigFactory.load().getConfig("http")
 
+  log.debug("Starting Actor System")
   implicit val system: ActorSystem = ActorSystem("Transporter")
   implicit val materializer: ActorMaterializer = ActorMaterializer()
 
+  log.debug("Initializing standard metrics")
   DefaultExports.initialize()
 
+  log.info("Starting HTTP Server")
   val server = Http().bindAndHandle(TransporterConsole.routes, interface = config.getString("interface"), port = config.getInt("port"))
 }
