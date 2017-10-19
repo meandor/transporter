4d66cc20a02e560c73e067790399a2d56b4d6d22
Daniel Schruhl <danielschruhl@gmail.com>
2017-10-19 18:57:38 +0200

    add shutdown routine

--- src/main/scala/de/transporter/Transporter.scala
+++ src/main/scala/de/transporter/Transporter.scala
@@ -7,19 +7,35 @@ import com.typesafe.config.ConfigFactory
 import io.prometheus.client.hotspot.DefaultExports
 import org.slf4j.LoggerFactory
 
+import scala.concurrent.ExecutionContext
+import scala.io.StdIn
+
 object Transporter extends App {
   val log = LoggerFactory.getLogger(Transporter.getClass)
   log.info("Starting Transporter")
   log.debug("Loading config")
-  val config = ConfigFactory.load().getConfig("http")
+  val config = ConfigFactory.load()
 
   log.debug("Starting Actor System")
   implicit val system: ActorSystem = ActorSystem("Transporter")
   implicit val materializer: ActorMaterializer = ActorMaterializer()
+  implicit val executionContext: ExecutionContext = system.dispatcher
 
   log.debug("Initializing standard metrics")
   DefaultExports.initialize()
 
-  log.info("Starting HTTP Server")
-  val server = Http().bindAndHandle(TransporterConsole.routes, interface = config.getString("interface"), port = config.getInt("port"))
+  val httpConfig = config.getConfig("http")
+  val httpPort = httpConfig.getInt("port")
+  val httpInterface = httpConfig.getString("interface")
+  val serverBindingFuture = Http().bindAndHandle(TransporterConsole.routes, interface = httpInterface, port = httpPort)
+  log.info(s"Started HTTP Server at $httpInterface:$httpPort")
+  log.info("Press RETURN to stop")
+  StdIn.readLine()
+
+  serverBindingFuture
+    .flatMap(_.unbind())
+    .onComplete { done =>
+      done.failed.map { ex => log.error("Failed unbinding", ex) }
+      system.terminate()
+    }
 }
